f317551a7380a94a9eb0c8bf7376249a0b847a98
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-11-27 20:55:24 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+// return the first two drivers in the array.
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2)
+}
+
+// return the last two drivers in the array.
+const returnLastTwoDrivers = function(drivers) {return drivers.slice(-2)}
+
+// an array containing two elements: the two functions that we previously defined (returnFirstTwoDrivers() and returnLastTwoDrivers()).
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(int) {
+    return function(fare) {
+        return fare * int
+    }
+
+}
+    
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers)
+}
+
